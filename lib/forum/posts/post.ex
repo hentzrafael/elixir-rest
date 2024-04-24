945cc0cafb8e5a2baa50a0555c0af4520ddbdb90
Rafael Hentz <rafaelhentz172@gmail.com>
2024-04-24 15:02:05 -0300

    feat: create posts mapping with user relation

--- lib/forum/posts/post.ex
+++ lib/forum/posts/post.ex
@@ -5,14 +5,23 @@ defmodule Forum.Posts.Post do
   schema "posts" do
     field :title, :string
     field :body, :string
+    belongs_to :user, Forum.Accounts.User
 
     timestamps(type: :utc_datetime)
   end
 
+  @spec changeset(
+          {map(), map()}
+          | %{
+              :__struct__ => atom() | %{:__changeset__ => map(), optional(any()) => any()},
+              optional(atom()) => any()
+            },
+          :invalid | %{optional(:__struct__) => none(), optional(atom() | binary()) => any()}
+        ) :: Ecto.Changeset.t()
   @doc false
   def changeset(post, attrs) do
     post
-    |> cast(attrs, [:body, :title])
-    |> validate_required([:body, :title])
+    |> cast(attrs, [:body, :title, :user_id])
+    |> validate_required([:body, :title, :user_id])
   end
 end
